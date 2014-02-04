2e5de04d6f471cce39962ad8b1b665f1db41480f
Maurice van Creij <maurice.vancreij@webqem.com>
2014-02-05 08:34:40 +1100

    Switched to grunt.js for pre-processing.

--- js/useful-scrolllock.js
+++ js/useful-scrolllock.js
@@ -1,74 +1,4 @@
 /*
-	Combined JavaScripts
-
-	@codekit-append "../src/useful.scrolllock.js"
-	@codekit-append "../../useful-positions/src/useful.positions.js"
-	@codekit-append "../../useful-polyfills/src/useful.polyfills.js"
-*/
-
-
-/* **********************************************
-     Begin useful.scrolllock.js
-********************************************** */
-
-/*
-	Source:
-	van Creij, Maurice (2012). "useful.scrolllock.js: Manages elements that float overtop of scrolling content.", version 20120606, http://www.woollymittens.nl/.
-
-	License:
-	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
-
-	Prerequisites:
-	<!--[if IE]>
-		<script src="./js/html5.js"></script>
-	<![endif]-->
-*/
-
-(function (useful) {
-
-	// invoke strict mode
-	"use strict";
-
-	// private functions
-	useful.Scrolllock = function (obj, cfg) {
-		// properties
-		this.obj = obj;
-		this.cfg = cfg;
-		// methods
-		this.start = function () {
-			var context = this;
-			// set the event handlers
-			window.addEventListener('scroll', function () { context.adjust(); }, false);
-			window.addEventListener('resize', function () { context.adjust(); }, false);
-			// measure the trigger position if none was given
-			this.cfg.threshold = this.cfg.threshold || useful.positions.object(this.obj);
-		};
-		this.adjust = function () {
-			// get the current scroll position
-			var scrolled = useful.positions.document();
-			// if scrolled far enough
-			if (scrolled.y > this.cfg.threshold.y || scrolled.x > this.cfg.threshold.x) {
-				// apply the scroll lock class
-				if (!this.obj.className.match(/scroll-locked/gi)) {
-					this.obj.className = this.obj.className.replace(/scroll-unlocked/g, '').replace(/  /g, ' ') + ' scroll-locked';
-				}
-			} else {
-				// remove the scroll lock style
-				if (!this.obj.className.match(/scroll-unlocked/gi)) {
-					this.obj.className = this.obj.className.replace(/scroll-locked/g, '').replace(/  /g, ' ') + ' scroll-unlocked';
-				}
-			}
-		};
-	};
-
-}(window.useful = window.useful || {}));
-
-
-/* **********************************************
-     Begin useful.positions.js
-********************************************** */
-
-/*
 	Source:
 	van Creij, Maurice (2012). "useful.positions.js: A library of useful functions to ease working with screen positions.", version 20121126, http://www.woollymittens.nl/.
 
@@ -85,12 +15,19 @@
 	var positions = positions || {};
 
 	// find the dimensions of the window
-	positions.window = function () {
+	positions.window = function (parent) {
 		// define a position object
 		var dimensions = {x : 0, y : 0};
-		// find the current dimensions of the window
-		dimensions.x = window.innerWidth || document.body.clientWidth;
-		dimensions.y = window.innerHeight || document.body.clientHeight;
+		// if an alternative was given to use as a window
+		if (parent && parent !== window) {
+			// find the current dimensions of surrogate window
+			dimensions.x = parent.offsetWidth;
+			dimensions.y = parent.offsetHeight;
+		} else {
+			// find the current dimensions of the window
+			dimensions.x = window.innerWidth || document.body.clientWidth;
+			dimensions.y = window.innerHeight || document.body.clientHeight;
+		}
 		// return the object
 		return dimensions;
 	};
@@ -100,7 +37,7 @@
 		// define a position object
 		var position = {x : 0, y : 0};
 		// find the current position in the document
-		if (parent) {
+		if (parent && parent !== window) {
 			position.x = parent.scrollLeft;
 			position.y = parent.scrollTop;
 		} else {
@@ -165,11 +102,6 @@
 	useful.positions.cursor = positions.cursor;
 
 }(window.useful = window.useful || {}));
-
-
-/* **********************************************
-     Begin useful.polyfills.js
-********************************************** */
 
 /*
 	Source:
@@ -321,5 +253,57 @@
 	polyfills.consoleLog();
 	polyfills.objectCreate();
 	polyfills.stringTrim();
+
+}(window.useful = window.useful || {}));
+
+/*
+	Source:
+	van Creij, Maurice (2012). "useful.scrolllock.js: Manages elements that float overtop of scrolling content.", version 20120606, http://www.woollymittens.nl/.
+
+	License:
+	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
+
+	Prerequisites:
+	<!--[if IE]>
+		<script src="./js/html5.js"></script>
+	<![endif]-->
+*/
+
+(function (useful) {
+
+	// invoke strict mode
+	"use strict";
+
+	// private functions
+	useful.Scrolllock = function (obj, cfg) {
+		// properties
+		this.obj = obj;
+		this.cfg = cfg;
+		// methods
+		this.start = function () {
+			var context = this;
+			// set the event handlers
+			window.addEventListener('scroll', function () { context.adjust(); }, false);
+			window.addEventListener('resize', function () { context.adjust(); }, false);
+			// measure the trigger position if none was given
+			this.cfg.threshold = this.cfg.threshold || useful.positions.object(this.obj);
+		};
+		this.adjust = function () {
+			// get the current scroll position
+			var scrolled = useful.positions.document();
+			// if scrolled far enough
+			if (scrolled.y > this.cfg.threshold.y || scrolled.x > this.cfg.threshold.x) {
+				// apply the scroll lock class
+				if (!this.obj.className.match(/scroll-locked/gi)) {
+					this.obj.className = this.obj.className.replace(/scroll-unlocked/g, '').replace(/  /g, ' ') + ' scroll-locked';
+				}
+			} else {
+				// remove the scroll lock style
+				if (!this.obj.className.match(/scroll-unlocked/gi)) {
+					this.obj.className = this.obj.className.replace(/scroll-locked/g, '').replace(/  /g, ' ') + ' scroll-unlocked';
+				}
+			}
+		};
+	};
 
 }(window.useful = window.useful || {}));
