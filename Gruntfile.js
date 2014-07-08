42089636811a0219419602071ae4d96bbf590521
Maurice van Creij <maurice.vancreij@webqem.com>
2014-07-08 14:13:18 +1000

    Reduced reliance on typography.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,9 @@ module.exports = function(grunt) {
 		'../useful-positions/src/js/useful-positions.js',
 		'../useful-polyfills/src/js/useful-polyfills.js'
 	];
+	var mixins = [
+		'../useful/src/scss/_mixins.scss'
+	];
 
 	// configuration.
 	var config = {
@@ -33,12 +36,18 @@ module.exports = function(grunt) {
 			}
 		},
 		copy: {
-			target: {
+			libs: {
 				flatten: true,
 				expand: true,
 				src: libs,
 				dest: './src/lib/'
-			}
+			},
+			mixins: {
+				flatten: true,
+				expand: true,
+				src: mixins,
+				dest: './src/scss/'
+			},
 		},
 		compass: {
 			dev : {
@@ -69,13 +78,33 @@ module.exports = function(grunt) {
 				src: ['./src/lib/*.js', './src/js/*.js'],
 				dest: './inc/js/useful-' + name + '.js'
 			}
+		},
+		uglify: {
+			all : {
+				src: ['./src/lib/*.js', './src/js/*.js'],
+				dest: './inc/js/useful-' + name + '.js'
+			}
+		},
+		font_optimizer: {
+			all: {
+				options: {
+					chars: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()-_+={}\\/":;><.,\'',
+					includeFeatures: ['kern']
+				},
+				files: {
+					'./inc/fonts/': ['./src/fonts/*.ttf']
+				}
+			}
+		},
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 version', 'ie 8', 'ie 9']
+			},
+			no_dest: {
+				src: '**/inc/css/*.css'
+			}
 		}
 	};
-
-	// named properties
-	config.uglify = {};
-	config.uglify.files = {};
-	config.uglify.files['./inc/js/useful-' + name + '.js'] = ['./src/lib/*.js', './src/js/*.js'];
 
 	// init
 	grunt.initConfig(config);
@@ -88,12 +117,15 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-font-optimizer');
+	grunt.loadNpmTasks('grunt-autoprefixer');
 
 	// tasts
 	grunt.registerTask('default', ['watch']);
 	grunt.registerTask('serve', ['connect', 'watch']);
-	grunt.registerTask('dev', ['compass', 'jshint', 'concat']);
-	grunt.registerTask('prod', ['compass', 'jshint', 'uglify']);
+	grunt.registerTask('dev', ['compass', 'autoprefixer', 'jshint', 'concat']);
+	grunt.registerTask('prod', ['compass', 'autoprefixer', 'jshint', 'uglify']);
 	grunt.registerTask('import', ['copy']);
+	grunt.registerTask('fonts', ['font_optimizer']);
 
 };
