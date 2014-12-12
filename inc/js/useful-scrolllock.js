f10b33e3beb9d1432ac281abc5cdf15219462eb4
Maurice van Creij <maurice.vancreij@webqem.com>
2014-12-12 11:31:51 +1100

    Improve readability of code.

--- inc/js/useful-scrolllock.js
+++ inc/js/useful-scrolllock.js
@@ -279,11 +279,11 @@ var useful = useful || {};
 })();
 
 /*
-	Source:
-	van Creij, Maurice (2014). "useful.positions.js: A library of useful functions to ease working with screen positions.", version 20141127, http://www.woollymittens.nl/.
+Source:
+van Creij, Maurice (2014). "useful.positions.js: A library of useful functions to ease working with screen positions.", version 20141127, http://www.woollymittens.nl/.
 
-	License:
-	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
+License:
+This work is licensed under a Creative Commons Attribution 3.0 Unported License.
 */
 
 // public object
@@ -325,15 +325,15 @@ var useful = useful || {};
 				position.y = parent.scrollTop;
 			} else {
 				position.x = (window.pageXOffset) ?
-					window.pageXOffset :
-					(document.documentElement) ?
-						document.documentElement.scrollLeft :
-						document.body.scrollLeft;
+				window.pageXOffset :
+				(document.documentElement) ?
+				document.documentElement.scrollLeft :
+				document.body.scrollLeft;
 				position.y = (window.pageYOffset) ?
-					window.pageYOffset :
-					(document.documentElement) ?
-						document.documentElement.scrollTop :
-						document.body.scrollTop;
+				window.pageYOffset :
+				(document.documentElement) ?
+				document.documentElement.scrollTop :
+				document.body.scrollTop;
 			}
 			// return the object
 			return position;
@@ -363,8 +363,16 @@ var useful = useful || {};
 			// define a position object
 			var position = {x : 0, y : 0};
 			// find the current position on the document
-			position.x = event.pageX || event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-			position.y = event.pageY || event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+			if (event.touches && event.touches[0]) {
+				position.x = event.touches[0].pageX;
+				position.y = event.touches[0].pageY;
+			} else if (event.pageX !== undefined) {
+				position.x = event.pageX;
+				position.y = event.pageY;
+			} else {
+				position.x = event.clientX + (document.documentElement.scrollLeft || document.body.scrollLeft);
+				position.y = event.clientY + (document.documentElement.scrollTop || document.body.scrollTop);
+			}
 			// if a parent was given
 			if (parent) {
 				// retrieve the position of the parent
@@ -394,50 +402,53 @@ var useful = useful || {};
 	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
 */
 
-// create the constructor if needed
+// create the global object if needed
 var useful = useful || {};
-useful.ScrollLock = useful.ScrollLock || function () {};
 
-// extend the constructor
-useful.ScrollLock.prototype.init = function (cfg) {
-	// properties
+// extend the global object
+useful.ScrollLock = function (config) {
+
+	// PROPERTIES
+
 	"use strict";
-	this.cfg = cfg;
-	this.obj = cfg.element;
-	// methods
-	this.events = function () {
-		var context = this;
+
+	// METHODS
+
+	this.init = function (config) {
+		// store the config
+		this.config = config;
+		this.element = config.element;
 		// set the event handlers
 		window.addEventListener('scroll', this.onReposition(), false);
 		window.addEventListener('resize', this.onReposition(), false);
 		// measure the trigger position if none was given
-		this.cfg.threshold = this.cfg.threshold || useful.positions.object(this.obj);
-		// disable the start function so it can't be started twice
-		this.init = function () {};
+		this.config.threshold = this.config.threshold || useful.positions.object(this.element);
+		// return the object
+		return this;
 	};
-	// events
+
+	// EVENTS
+
 	this.onReposition = function () {
 		var _this = this;
 		return function () {
 			// get the current scroll position
 			var scrolled = useful.positions.document();
 			// if scrolled far enough
-			if (scrolled.y > _this.cfg.threshold.y || scrolled.x > _this.cfg.threshold.x) {
+			if (scrolled.y > _this.config.threshold.y || scrolled.x > _this.config.threshold.x) {
 				// apply the scroll lock class
-				if (!_this.obj.className.match(/scroll-locked/gi)) {
-					_this.obj.className = _this.obj.className.replace(/scroll-unlocked/g, '').replace(/  /g, ' ') + ' scroll-locked';
+				if (!_this.element.className.match(/scroll-locked/gi)) {
+					_this.element.className = _this.element.className.replace(/scroll-unlocked/g, '').replace(/  /g, ' ') + ' scroll-locked';
 				}
 			} else {
 				// remove the scroll lock style
-				if (!_this.obj.className.match(/scroll-unlocked/gi)) {
-					_this.obj.className = _this.obj.className.replace(/scroll-locked/g, '').replace(/  /g, ' ') + ' scroll-unlocked';
+				if (!_this.element.className.match(/scroll-unlocked/gi)) {
+					_this.element.className = _this.element.className.replace(/scroll-locked/g, '').replace(/  /g, ' ') + ' scroll-unlocked';
 				}
 			}
 		};
 	};
-	// go
-	this.events();
-	return this;
+
 };
 
 // return as a require.js module
--- src/js/useful-scrolllock.js
+++ src/js/useful-scrolllock.js
@@ -6,50 +6,53 @@
 	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
 */
 
-// create the constructor if needed
+// create the global object if needed
 var useful = useful || {};
-useful.ScrollLock = useful.ScrollLock || function () {};
 
-// extend the constructor
-useful.ScrollLock.prototype.init = function (cfg) {
-	// properties
+// extend the global object
+useful.ScrollLock = function (config) {
+
+	// PROPERTIES
+
 	"use strict";
-	this.cfg = cfg;
-	this.obj = cfg.element;
-	// methods
-	this.events = function () {
-		var context = this;
+
+	// METHODS
+
+	this.init = function (config) {
+		// store the config
+		this.config = config;
+		this.element = config.element;
 		// set the event handlers
 		window.addEventListener('scroll', this.onReposition(), false);
 		window.addEventListener('resize', this.onReposition(), false);
 		// measure the trigger position if none was given
-		this.cfg.threshold = this.cfg.threshold || useful.positions.object(this.obj);
-		// disable the start function so it can't be started twice
-		this.init = function () {};
+		this.config.threshold = this.config.threshold || useful.positions.object(this.element);
+		// return the object
+		return this;
 	};
-	// events
+
+	// EVENTS
+
 	this.onReposition = function () {
 		var _this = this;
 		return function () {
 			// get the current scroll position
 			var scrolled = useful.positions.document();
 			// if scrolled far enough
-			if (scrolled.y > _this.cfg.threshold.y || scrolled.x > _this.cfg.threshold.x) {
+			if (scrolled.y > _this.config.threshold.y || scrolled.x > _this.config.threshold.x) {
 				// apply the scroll lock class
-				if (!_this.obj.className.match(/scroll-locked/gi)) {
-					_this.obj.className = _this.obj.className.replace(/scroll-unlocked/g, '').replace(/  /g, ' ') + ' scroll-locked';
+				if (!_this.element.className.match(/scroll-locked/gi)) {
+					_this.element.className = _this.element.className.replace(/scroll-unlocked/g, '').replace(/  /g, ' ') + ' scroll-locked';
 				}
 			} else {
 				// remove the scroll lock style
-				if (!_this.obj.className.match(/scroll-unlocked/gi)) {
-					_this.obj.className = _this.obj.className.replace(/scroll-locked/g, '').replace(/  /g, ' ') + ' scroll-unlocked';
+				if (!_this.element.className.match(/scroll-unlocked/gi)) {
+					_this.element.className = _this.element.className.replace(/scroll-locked/g, '').replace(/  /g, ' ') + ' scroll-unlocked';
 				}
 			}
 		};
 	};
-	// go
-	this.events();
-	return this;
+
 };
 
 // return as a require.js module
